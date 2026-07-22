d78bd5aa994f9970bac684b9546d9e033e62ecb3
JoaoMonteiro00 <jpereiramonteiro@ipvc.pt>
2026-07-22 22:39:45 +0100

    Modulo 3 - Gestão de alertas

--- include/alertas.h
+++ include/alertas.h
@@ -1,23 +1,66 @@
 #ifndef ALERTAS_H
 #define ALERTAS_H
 
+#include "sensores.h"
 
 typedef struct Alerta
 {
     int identificador;
+
     char dataHoraCriacao[30];
     char codigoSensor[30];
     char descricao[200];
     char estadoObservado[20];
+
     int severidade;
+
     char estadoAtendimento[20];
     char dataHoraConclusao[30];
-    
+
     struct Alerta *seguinteLista;
     struct Alerta *seguinteFila;
 
 } Alerta;
 
 
+int criarAlertaManual(
+    Alerta **lista,
+    Alerta **fila,
+    char codigoSensor[],
+    char descricao[],
+    char estadoObservado[],
+    int severidade
+);
 
-endif
+int criarAlertaAutomatico(
+    Alerta **lista,
+    Alerta **fila,
+    char codigoSensor[],
+    char descricao[],
+    char estadoObservado[],
+    int severidade
+);
+
+int processarProximoAlerta(Alerta **fila);
+
+int concluirAlerta(
+    Alerta *lista,
+    int identificador
+);
+
+void listarAlertasPorEstado(
+    Alerta *lista,
+    char estado[]
+);
+
+int pesquisarAlertaPorId(
+    Alerta *lista,
+    int identificador
+);
+
+int pesquisarAlertasPorSensor(
+    Alerta *lista,
+    char codigoSensor[]
+);
+
+#endif
--- src/alertas.c
+++ src/alertas.c
@@ -1,10 +1,330 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>   //Precisamos guardar data e hora
 
 #include "../include/alertas.h"
 
 
-Alerta *criarAlerta(Sensor *sensor); {
-  
+//Guardar data e hora atual
+static void obterDataHora(char data[])
+{
+    time_t agora;
+    struct tm *info;
+
+    agora = time(NULL);
+    info = localtime(&agora);
+
+    strftime(data, 30, "%d/%m/%Y %H:%M:%S", info);
+}
+
+//Função auxiliar usada posteriormente na criação manual e automática de alertas
+static Alerta *criarAlerta(
+    char codigoSensor[],
+    char descricao[],
+    char estadoObservado[],
+    int severidade
+)
+{
+    static int proximoId = 1;
+
+    Alerta *novo;
+
+    novo = malloc(sizeof(Alerta));
+
+    if (novo == NULL)
+    {
+        return NULL;
+    }
+
+    novo->identificador = proximoId;
+    proximoId++;
+
+    obterDataHora(novo->dataHoraCriacao);
+
+    strcpy(novo->codigoSensor, codigoSensor);
+    strcpy(novo->descricao, descricao);
+    strcpy(novo->estadoObservado, estadoObservado);
+
+    novo->severidade = severidade;
+
+    strcpy(novo->estadoAtendimento, "Pendente");
+    strcpy(novo->dataHoraConclusao, "");
+
+    novo->seguinteLista = NULL;
+    novo->seguinteFila = NULL;
+
+    return novo;
+}
+
+static int registarAlerta(
+    Alerta **lista,
+    Alerta **fila,
+    char codigoSensor[],
+    char descricao[],
+    char estadoObservado[],
+    int severidade
+)
+{
+    Alerta *novo;
+    Alerta *atual;
+
+
+    //Evita alertas repetidos
+    if (existeAlertaAtivo(*lista, codigoSensor, estadoObservado, severidade))
+    {
+      return 0;
+    }
+
+    novo = criarAlerta(
+        codigoSensor,
+        descricao,
+        estadoObservado,
+        severidade
+    );
+
+    if (novo == NULL)
+    {
+        return 0;
+    }
+
+    // Adicionar à lista
+    if (*lista == NULL)
+    {
+        *lista = novo;
+    }
+    else
+    {
+        atual = *lista;
+
+        while (atual->seguinteLista != NULL)
+        {
+            atual = atual->seguinteLista;
+        }
+
+        atual->seguinteLista = novo;
+    }
+
+    // Adicionar à fila
+    if (*fila == NULL)
+    {
+        *fila = novo;
+    }
+    else
+    {
+        atual = *fila;
+
+        while (atual->seguinteFila != NULL)
+        {
+            atual = atual->seguinteFila;
+        }
+
+        atual->seguinteFila = novo;
+    }
+
+    return 1;
+}
+
+int criarAlertaManual(
+    Alerta **lista,
+    Alerta **fila,
+    char codigoSensor[],
+    char descricao[],
+    char estadoObservado[],
+    int severidade
+)
+{
+    return registarAlerta(
+        lista,
+        fila,
+        codigoSensor,
+        descricao,
+        estadoObservado,
+        severidade
+    );
+}
+
+int criarAlertaAutomatico(
+    Alerta **lista,
+    Alerta **fila,
+    char codigoSensor[],
+    char descricao[],
+    char estadoObservado[],
+    int severidade
+)
+{
+    //Validação alerta automático
+    if (strcmp(estadoObservado, "NORMAL") == 0 && severidade == 0)
+    {
+        return 0;
+    }
+
+    return registarAlerta(
+        lista,
+        fila,
+        codigoSensor,
+        descricao,
+        estadoObservado,
+        severidade
+    );
+}
+
+
+static int existeAlertaAtivo(
+    Alerta *lista,
+    char codigoSensor[],
+    char estadoObservado[],
+    int severidade
+)
+{
+    Alerta *atual;
+
+    atual = lista;
+
+    while (atual != NULL)
+    {   
+        // Verifica se os dados do alerta coincidem
+        if (strcmp(atual->codigoSensor, codigoSensor) == 0 && strcmp(atual->estadoObservado, estadoObservado) == 0 &&atual->severidade == severidade && (strcmp(atual->estadoAtendimento, "Pendente") == 0 || strcmp(atual->estadoAtendimento, "Em Curso") == 0))
+        {
+            return 1;
+        }
+
+        atual = atual->seguinteLista;
+    }
+
+    return 0;
+}
+
+int processarProximoAlerta(Alerta **fila)
+{
+    Alerta *atual;
+
+    if (*fila == NULL)
+    {
+        return 0;
+    }
+
+    atual = *fila;
+
+    *fila = atual->seguinteFila;
+
+    atual->seguinteFila = NULL;
+
+    strcpy(atual->estadoAtendimento, "Em Curso");
+
+    return 1;
+}
+
+int concluirAlerta(Alerta *lista, int identificador)
+{
+    Alerta *atual;
+
+    atual = lista;
+
+    while (atual != NULL)
+    {
+        if (
+            atual->identificador == identificador &&             //Percorre a lista e procura identificador e estado em Curso
+            strcmp(atual->estadoAtendimento, "Em Curso") == 0
+        )
+        {
+            strcpy(atual->estadoAtendimento, "Concluido");      //Muda o estado para Concluído
+
+            obterDataHora(atual->dataHoraConclusao);
+
+            return 1;
+        }
+
+        atual = atual->seguinteLista;
+    }
+
+    return 0;
+}
+
+static void mostrarAlerta(Alerta *alerta)
+{
+    printf("\nIdentificador: %d\n", alerta->identificador);
+    printf("Data e hora de criacao: %s\n", alerta->dataHoraCriacao);
+    printf("Codigo do sensor: %s\n", alerta->codigoSensor);
+    printf("Descricao: %s\n", alerta->descricao);
+    printf("Estado observado: %s\n", alerta->estadoObservado);
+    printf("Severidade: %d\n", alerta->severidade);
+    printf("Estado de atendimento: %s\n", alerta->estadoAtendimento);
+
+    if (strcmp(alerta->estadoAtendimento, "Concluido") == 0)
+    {
+        printf("Data e hora de conclusao: %s\n",
+               alerta->dataHoraConclusao);
+    }
+}
+
+void listarAlertasPorEstado(Alerta *lista, char estado[])
+{
+    Alerta *atual;
+    int encontrou;
+
+    atual = lista;
+    encontrou = 0;
+
+    while (atual != NULL)
+    {
+        if (strcmp(atual->estadoAtendimento, estado) == 0)
+        {
+            mostrarAlerta(atual);
+            encontrou = 1;
+        }
+
+        atual = atual->seguinteLista;
+    }
+
+    if (encontrou == 0)
+    {
+        printf("\nNao existem alertas com o estado %s.\n", estado);
+    }
+}
+
+int pesquisarAlertaPorId(Alerta *lista, int identificador)
+{
+    Alerta *atual;
+
+    atual = lista;
+
+    while (atual != NULL)
+    {
+        if (atual->identificador == identificador)
+        {
+            mostrarAlerta(atual);
+            return 1;
+        }
+
+        atual = atual->seguinteLista;
+    }
+
+    return 0;
+}
+
+
+int pesquisarAlertasPorSensor(
+    Alerta *lista,
+    char codigoSensor[]
+)
+{
+    Alerta *atual;
+    int encontrou;
+
+    atual = lista;
+    encontrou = 0;
+
+    while (atual != NULL)
+    {
+        if (strcmp(atual->codigoSensor, codigoSensor) == 0)
+        {
+            mostrarAlerta(atual);
+            encontrou = 1;
+        }
+
+        atual = atual->seguinteLista;
+    }
+
+    return encontrou;
 }
